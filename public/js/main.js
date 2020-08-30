c58102f5554349b1fde575630a39c13edacf071b
Andy Bacon <bacon.andrew@gmail.com>
2020-08-30 15:11:23 -0700

    Added back button, hid navigator on machine pages, display door panel answer on door panel page, display drink recipes on dispenser page.

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
       var roomCode = '';
+      var currentPageId = '';
 
       const ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
 
       document.querySelector('#navigator').style.display = "none";
+      document.querySelector('#machine-door-panel').style.display = "none";
+      document.querySelector('#machine-storage-box').style.display = "none";
+      document.querySelector('#machine-drink-dispenser').style.display = "none";
+      document.querySelector('#back-button').style.display = "none";
 
       ws.onopen = () => {
         console.log('Connection opened!');
@@ -18,14 +23,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
 
         if (webAppMessage.messageType == "JOINED_ROOM"){
-          fetch('/navigator.html')
-          .then((response) => {
-              return response.text();
-          })
-          .then((body) => {
-              document.querySelector('#connect').style.display = "none";
-              document.querySelector('#navigator').style.display = "block";
-          });
+          document.querySelector('#connect').style.display = "none";
+          document.querySelector('#navigator').style.display = "block";
+
+          var message = {
+            messageType: 'WEB_APP_PLAYER_JOINED',
+            roomCode: roomCode,
+          };
+          ws.send(JSON.stringify(message));
         }
 
         if (webAppMessage.messageType == "DOOR_PANEL_SETUP"){
@@ -63,6 +68,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
       const botBartenderButton = document.querySelector('#bot-bartender-button');
       const testCssBtn = document.querySelector('#test-css-button');
 
+      document.querySelector('#back-button').addEventListener('click', function(){
+        document.querySelector('#navigator').style.display = "block";
+        document.querySelector(currentPageId).style.display = "none";
+        document.querySelector('#back-button').style.display = "none";
+      });
+
       connectBtn.addEventListener('click', function(){
         console.log("connect button pressed");
 
@@ -87,54 +98,48 @@ document.addEventListener("DOMContentLoaded", function(event) {
       });
 
       machineDoorPanelButton.addEventListener('click', function(){
-        fetch('/machineDoorPanel.html')
-          .then((response) => {
-              return response.text();
-          })
-          .then((body) => {
-              document.querySelector('#game-content').innerHTML = body;
-              
-              var test = {
-                messageType: 'REQUEST_DOOR_PANEL_SETUP',
-                roomCode: roomCode,
-              };
-      
-              ws.send(JSON.stringify(test));
-          });
+
+        document.querySelector('#navigator').style.display = "none";
+        document.querySelector('#machine-door-panel').style.display = "block";
+        document.querySelector('#back-button').style.display = "block";
+        currentPageId = '#machine-door-panel';
+
+        var test = {
+          messageType: 'REQUEST_DOOR_PANEL_SETUP',
+          roomCode: roomCode,
+        };
+
+        ws.send(JSON.stringify(test));
       });
 
       machineStorageBoxButton.addEventListener('click', function(){
-        fetch('/machineStorageBox.html')
-          .then((response) => {
-              return response.text();
-          })
-          .then((body) => {
-              document.querySelector('#game-content').innerHTML = body;
-              
-              var test = {
-                messageType: 'REQUEST_STORAGE_BOX_SETUP',
-                roomCode: roomCode,
-              };
-      
-              ws.send(JSON.stringify(test));
-          });
+
+        document.querySelector('#navigator').style.display = "none";
+        document.querySelector('#machine-storage-box').style.display = "block";
+        document.querySelector('#back-button').style.display = "block";
+        currentPageId = '#machine-storage-box';
+
+        var test = {
+          messageType: 'REQUEST_STORAGE_BOX_SETUP',
+          roomCode: roomCode,
+        };
+
+        ws.send(JSON.stringify(test));
       });
 
       machineDrinkDispenserButton.addEventListener('click', function(){
-        fetch('/machineDrinkDispenser.html')
-          .then((response) => {
-              return response.text();
-          })
-          .then((body) => {
-              document.querySelector('#game-content').innerHTML = body;
-                
-              var test = {
-                messageType: 'REQUEST_DRINK_DISPENSER_SETUP',
-                roomCode: roomCode,
-              };
 
-              ws.send(JSON.stringify(test));
-          });
+        document.querySelector('#navigator').style.display = "none";
+        document.querySelector('#machine-drink-dispenser').style.display = "block";
+        document.querySelector('#back-button').style.display = "block";
+        currentPageId = '#machine-drink-dispenser';
+
+        var test = {
+          messageType: 'REQUEST_DRINK_DISPENSER_SETUP',
+          roomCode: roomCode,
+        };
+
+        ws.send(JSON.stringify(test));
       });
 
       botBartenderButton.addEventListener('click', function(){
@@ -210,6 +215,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
           document.querySelector('#puzzle-button-' + i).addEventListener('click',function(){doorPanelButtonPressed(i)});
           document.querySelector('#puzzle-button-' + i).innerHTML = webAppSetup.puzzleInputs[i].symbolIndex;
         }
+
+        for(var i = 0; i < answerSequence.length; i++){
+          document.querySelector('#answer-' + i).innerHTML = answerSequence[i].symbolIndex;
+          document.querySelector('#answer-' + i).style.color = "#" + webAppSetup.colors[answerSequence[i].colorIndex];
+        }
+
       }
 
       function setupStorageBox(webAppSetup){
@@ -233,6 +244,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
 
       function setupDrinkDispenser(webAppSetup){    
+        var drinkRecipes = webAppSetup.drinkRecipes;       
+        document.querySelector('#drink-recipes').innerHTML = drinkRecipes;
+
         for(let i = 0; i < 6; i++){
           document.querySelector('#puzzle-button-' + i).addEventListener('click',function(){drinkDispenserButtonPressed(i)});
         }
@@ -240,7 +254,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
       function setupBartenderBot(webAppSetup){
         var drinkRecipes = webAppSetup.drinkRecipes;       
-        document.querySelector('#bot-content').innerHTML = drinkRecipes;
+        document.querySelector('#drink-recipes').innerHTML = drinkRecipes;
       }
 
       function setupGuardBot(webAppSetup){

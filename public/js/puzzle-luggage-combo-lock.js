99ec1798038a951e69bd4f46b5bafa2e23f12604
lisatrac-gamedev <68930559+lisatrac-gamedev@users.noreply.github.com>
2020-10-14 14:38:16 -0700

    Made luggage searchable by tag id instead of just list of them

--- public/js/puzzle-luggage-combo-lock.js
+++ public/js/puzzle-luggage-combo-lock.js
@@ -2,19 +2,81 @@ var puzzleLuggageComboLock = new Puzzle("PUZZLE_LUGGAGECOMBOLOCK", "./html/puzzl
 
     function (webAppSetup) {
 
+        //store luggage tags / hints for reference when entering tag serial code to find luggage to hack
+        var luggageAllTags = webAppSetup.luggageAllTags;
+        var luggagePuzzleTags = webAppSetup.luggagePuzzleTags;
+        var luggagePuzzleHints = webAppSetup.luggagePuzzleHints;
+        var currentLookupTag = -1;
+
+        //start with below inputs hidden
+        document.querySelector("#input-luggage-code").value = "";
+        document.querySelector("#input-luggage-code").style.display = "none";
+        document.querySelector("#button-send-code").style.display = "none";
+
+
+        //search for matching luggage tag id to show security question / input field for entering passcode
+        document.querySelector("#button-search-id").addEventListener('click', function()
+        {
+            //if there is a search result
+            if (luggageAllTags.includes(document.querySelector("#input-serial-id").value))
+            {
+                currentLookupTag = luggageAllTags.indexOf(document.querySelector("#input-serial-id").value);
+
+                //check if has puzzle associated with tag
+                if (luggagePuzzleTags.includes(document.querySelector("#input-serial-id").value))
+                {
+                    //show security question, input field, send button
+                    document.querySelector("#result-search").innerHTML = luggagePuzzleHints[currentLookupTag];
+                    document.querySelector("#input-luggage-code").value = "";
+                    document.querySelector("#input-luggage-code").style.display = "block";
+                    document.querySelector("#button-send-code").style.display = "block";
+                    document.querySelector("#response").innerHTML = "";
+                }
+                else
+                {
+                    //hide security question, input field, send button
+                    document.querySelector("#result-search").innerHTML = "- ACCESS DENIED -<br>- HIGH LEVEL ENCRYPTION -";
+                    document.querySelector("#input-luggage-code").value = "";
+                    document.querySelector("#input-luggage-code").style.display = "none";
+                    document.querySelector("#button-send-code").style.display = "none";
+                }
+            }
+            else
+            {
+                //hide security question, input field, send button
+                document.querySelector("#result-search").innerHTML = "- NO RESULTS -";
+                document.querySelector("#input-luggage-code").value = "";
+                document.querySelector("#input-luggage-code").style.display = "none";
+                document.querySelector("#button-send-code").style.display = "none";
+            }
+        });
+
+
+        //send luggage passcode and associated tag id to unity for unlocking
+        document.querySelector("#button-send-code").addEventListener('click', function()
+        {
+            var webAppData = {
+                luggageTag: luggageAllTags[currentLookupTag],
+                passcode: document.querySelector("#input-luggage-code").value.toUpperCase()
+            }
+        
+            puzzleLuggageComboLock.sendData(webAppData);
+        });
+
+        //OLD SETUP TO SHOW MULTIPLE LUGGAGE INPUT FIELDS FOR EACH LUGGAGE
         //generate list display by iterating through lists of luggage tags and security hints
-        for (var index = 0; index < webAppSetup.luggageTags.length; index++)
+        /*for (var index = 0; index < webAppSetup.luggagePuzzleTags.length; index++)
         {
             //luggage tag
             var tag = document.createElement("P");
             tag.setAttribute('id', "tag-" + index);
-            tag.innerHTML = "<br>LUGGAGE TAG: " + webAppSetup.luggageTags[index];
+            tag.innerHTML = "<br>LUGGAGE TAG: " + webAppSetup.luggagePuzzleTags[index];
             document.querySelector('#div-luggage').appendChild(tag);
     
             //security question hint
             var hint = document.createElement("P");
             hint.setAttribute('id', "hint-" + index);
-            hint.innerHTML = "SECURITY QUESTION: " + webAppSetup.luggageHints[index];
+            hint.innerHTML = "SECURITY QUESTION: " + webAppSetup.luggagePuzzleHints[index];
             document.querySelector('#div-luggage').appendChild(hint);
     
             //input field
@@ -31,8 +93,8 @@ var puzzleLuggageComboLock = new Puzzle("PUZZLE_LUGGAGECOMBOLOCK", "./html/puzzl
             button.innerHTML = "SEND";
             button.addEventListener('click', function()
             {
-                //webAppSetup.luggageTags[index],
-                //document.getElementById('#input-' + index).value.toUpperCase
+                //webAppSetup.luggagePuzzleTags[index],
+                //document.querySelector('#input-' + index).value.toUpperCase()
                 var webAppData = {
                     luggageTag: "982",
                     passcode: "fudge"
@@ -47,7 +109,7 @@ var puzzleLuggageComboLock = new Puzzle("PUZZLE_LUGGAGECOMBOLOCK", "./html/puzzl
             response.setAttribute('id', "response-" + index);
             response.innerHTML = "LOCKED";
             document.querySelector('#div-luggage').appendChild(response);
-        }
+        }*/
 
     },
 
@@ -60,7 +122,6 @@ var puzzleLuggageComboLock = new Puzzle("PUZZLE_LUGGAGECOMBOLOCK", "./html/puzzl
             message = "UNLOCKED"
         }
 
-        //update locked state for luggage
-        document.getElementById('#response-' + webAppResponse.luggageIndex).innerHTML = message;
+        document.querySelector("#response").innerHTML = message;
     }
 );

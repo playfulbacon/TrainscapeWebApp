ad924d9d45de4adb997223f7420de82269a7ab09
lisatrac-gamedev <68930559+lisatrac-gamedev@users.noreply.github.com>
2020-10-21 20:34:44 -0700

    Removed cabin doors from being unlocked by webapp

--- public/js/puzzle-train-settings.js
+++ public/js/puzzle-train-settings.js
@@ -2,47 +2,12 @@ var puzzleTrainSettings = new Puzzle("PUZZLE_TRAINSETTINGS", "./html/puzzle-trai
 
     function (webAppSetup) {
 
-        //setup door codes display and lock/unlock buttons
-        for (var i = 0; i < webAppSetup.cabinDoorCodes.length; i++)
-        {
-            //door code display
-            var code = document.createElement("P");
-            code.setAttribute('id', "door-code-" + i);
-            code.innerHTML = "DOOR " + webAppSetup.cabinDoorCodes[i] + ":";
-            document.querySelector('#div-door-locks').appendChild(code);
-
-            //door code button
-            var button = document.createElement("BUTTON");
-            button.setAttribute('id', "button-lock-" + i);
-            button.innerHTML = webAppSetup.cabinDoorUnlocks[i] ? "LOCK DOOR" : "UNLOCK DOOR";
-            button.addEventListener('click', buttonAssignment(button, i));
-            document.querySelector('#div-door-locks').appendChild(button);
-        }
-        //need external function for proper door index to be passed through (otherwise it takes the last iteration of 'i')
-        function buttonAssignment(door, doorIndex) {
-            return function(){
-                door.innerHTML = (door.innerHTML === "UNLOCK DOOR") ? "LOCK DOOR" : "UNLOCK DOOR";
-
-                var webAppData = {
-                    cabinDoorUpdate: true,
-                    cabinDoorIndex: doorIndex,
-                    musicUpdate: false,
-                    musicStateChange: false,
-                    musicTrackChange: false
-                }
-            
-                puzzleTrainSettings.sendData(webAppData);
-            }
-        }
-
         //setup music toggles
         document.querySelector('#button-music-toggle').addEventListener('click', function()
         {
             document.querySelector('#button-music-toggle').innerHTML = (document.querySelector('#button-music-toggle').innerHTML === "PLAY") ? "STOP" : "PLAY";
 
             var webAppData = {
-                cabinDoorUpdate: false,
-                cabinDoorIndex: 0,
                 musicUpdate: true,
                 musicStateChange: true,
                 musicTrackChange: false
@@ -55,8 +20,6 @@ var puzzleTrainSettings = new Puzzle("PUZZLE_TRAINSETTINGS", "./html/puzzle-trai
             document.querySelector('#button-music-toggle').innerHTML = "STOP";
 
             var webAppData = {
-                cabinDoorUpdate: false,
-                cabinDoorIndex: 0,
                 musicUpdate: true,
                 musicStateChange: false,
                 musicTrackChange: true

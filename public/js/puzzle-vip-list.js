e626b9e9001567443e63446656102d8936b3650c
lisatrac-gamedev <68930559+lisatrac-gamedev@users.noreply.github.com>
2020-11-13 16:00:29 -0800

    Added security bot password protection puzzle first before can view vib list

--- public/js/puzzle-vip-list.js
+++ public/js/puzzle-vip-list.js
@@ -2,6 +2,28 @@ var puzzleVipList = new Puzzle("VIP_LIST", "./html/puzzle-vip-list.html",
 
     // setup callback
     function (webAppSetup) {
+        //show security bot password protection first, then once correct, only show vib list from then on
+        document.querySelector("#div-security-passcode").style.display = "block";
+        document.querySelector("#div-vip-list").style.display = "none";
+
+        //security bot password protected
+        var securityBotPasscode = webAppSetup.securityBotPasscode;
+        
+        document.querySelector("#passcode-button").addEventListener("click", function()
+        {
+            if (document.querySelector("#passcode-input").value.toUpperCase() === securityBotPasscode)
+            {
+                document.querySelector("#passcode-result").innerHTML = "- CORRECT PASSWORD -";
+                document.querySelector("#div-security-passcode").style.display = "none";
+                document.querySelector("#div-vip-list").style.display = "block";
+            }
+            else
+            {
+                document.querySelector("#passcode-result").innerHTML = "- INCORRECT PASSWORD -";
+            }
+        });
+
+        //vip list setup
         var defaultNames = webAppSetup.defaultNames;
 
         var table = document.getElementById("vip-list-table");

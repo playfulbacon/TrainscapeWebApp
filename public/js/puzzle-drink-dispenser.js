6fea85901806d0dbbf3a7b1879eaba1045727dbb
lisatrac-gamedev <68930559+lisatrac-gamedev@users.noreply.github.com>
2020-12-15 14:08:49 -0800

    Disabling drink recipes displayed on web app side

--- public/js/puzzle-drink-dispenser.js
+++ public/js/puzzle-drink-dispenser.js
@@ -1,8 +1,8 @@
 var drinkDispenserPuzzle = new Puzzle("DRINK_DISPENSER", "Drink Dispenser", "./html/puzzle-drink-dispenser.html", 
 
     function (webAppSetup) {
-    var drinkRecipes = webAppSetup.drinkRecipes;       
-    document.querySelector('#drink-recipes').innerHTML = drinkRecipes;
+    //var drinkRecipes = webAppSetup.drinkRecipes;       
+    //document.querySelector('#drink-recipes').innerHTML = drinkRecipes;
 
     for(let i = 0; i < 6; i++){
         document.querySelector('#drink-button-' + i).addEventListener('click',function()
